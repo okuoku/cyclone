514c404bf4a80159c8bc9edd18b65fc1d21978cb
Justin Ethier <justin.ethier@gmail.com>
2015-03-10 22:24:29 -0400

    Debug adding

--- eval.scm
+++ eval.scm
@@ -111,6 +111,7 @@
 (define (frame-variables frame) (car frame))
 (define (frame-values frame) (cdr frame))
 (define (add-binding-to-frame! var val frame)
+  (write (list 'add-binding var val frame))
   (set-car! frame (cons var (car frame)))
   (set-cdr! frame (cons val (cdr frame))))
 
